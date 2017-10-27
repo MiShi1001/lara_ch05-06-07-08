c994781a78e97b3d56c850645ec8aced15cde36d
SU-RU <oooaa0727@gmail.com>
2017-10-27 09:54:46 +0800

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
@@ -48,3 +48,15 @@ Route::get('student/{student_no}',function ($student_no){
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 }) -> where(['subject' => '(chinese|english|math)']);
+*/
+//路由命名
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix' => 'student'],function(){
+    Route::get('{student_no}',['as' => 'student', 'uses' => function ($student_no) {
+        return '學號：' . $student_no;
+    }]);
+    Route::get('{student_no}/score/{subject?}',['as' => 'student.score',
+        'uses' => function ($student_no, $subject = null) {
+            return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
+        }])->where(['subject' => '(chinese|english|math)']);
+});
