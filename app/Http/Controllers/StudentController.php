e56b4b3fb931c79fbb60976abf57f1a1974d80b4
SU-RU <oooaa0727@gmail.com>
2017-11-21 21:15:10 +0800

    練習五_2修改 StudentController 之 getStudentData()方法及 getStudentScore()方法 (已寫入)練習五_在 StudentController 引用 Student model

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,22 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Student;
 use View;
 
 
+use App\Student;
 class StudentController extends Controller
 {
     public function getStudentData($student_no){
-        return View::make('score',[
-            'student_no'=>$student_no,
+        $student=Student::where('no',$student_no)->firstOrFall();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>null
         ]);
 
     }
     public function getStudentScore($student_no,$subject=null){
-        return View::make('score',[
-            'student_no'=>$student_no,
+        $student=Student::where('no',$student_no)->firstOrFall();
+        return View::make('student',[
+            'student'=>$student,
+            'user'=>$student->user,
+            'score'=>$student->score,
             'subject'=>$subject
         ]);
 
