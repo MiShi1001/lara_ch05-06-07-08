85c3f0d11bad57c799fd02b20111c4acb4d70541
SU-RU <oooaa0727@gmail.com>
2017-11-21 20:43:04 +0800

    練習三_2建立 Student 及 User 兩個 model 間的一對一關係

--- app/Student.php
+++ app/Student.php
@@ -3,12 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use \App\User;
 class Student extends Model
 {
     protected $table = 'students';
 
     public function score(){
             return $this->hasOne(Score::class);
+     }
+
+    public function user(){
+                return $this->belongsTo(User::class);
      }
 }
