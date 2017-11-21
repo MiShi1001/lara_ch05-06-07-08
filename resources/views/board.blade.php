134c2cce9413538fb20ccb2fda142da20de3e666
SU-RU <oooaa0727@gmail.com>
2017-11-21 22:45:25 +0800

    board route->OK

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -55,7 +55,7 @@
 								</td>
 
 								<td>
-									<a class="btn btn-default btn-sm" href="{{route('student',['student_no'=>'s1234567890'])}}">查看學生資料</a>
+									<a class="btn btn-default btn-sm" href="{{route('score')}}">查看學生資料</a>
 								</td>
 
 							</tr>
