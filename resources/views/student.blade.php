7bbe01385ca7a9176df099ae07107e76d8d2014f
SU-RU <oooaa0727@gmail.com>
2017-11-21 21:19:33 +0800

    練習五_3拷貝原有 score 視圖 (score.blade.php) 成為 student.blade.php，並 修改

---
+++ resources/views/student.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.master')
+
+@section('title','詳細資料')
+
+@section('content')
+
+	<div class="page-header">
+			<h1>詳細資料</h1>
+		</div>
+		<div class="row">
+			<p>
+				信箱：123456@ncut.edu.tw
+			</p>
+			<p>
+				學號：123456
+			</p>
+			<p>
+				姓名：小明
+			</p>
+			<p>
+				電話：0912345678
+			</p>
+			<p>
+				國文：60
+			</p>
+			<p>
+				英文：60
+			</p>
+			<p>
+				數學：60
+			</p>
+		</div>
+
+
+@stop
